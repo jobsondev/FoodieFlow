fd19653bf7b5968b23f13fae78b65183ae4e6974
EDERSON RIBEIRO PAZ <edersonpaz.jbz@gmail.com>
2024-01-29 17:43:00 -0300

    ajustes de conexao e parametrizacao dos pods

--- app/infrastructure/database.py
+++ app/infrastructure/database.py
@@ -1,24 +1,26 @@
+import os
+
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.dialects.postgresql import VARCHAR, FLOAT, INTEGER
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import text
-
+from dotenv import load_dotenv
 from decouple import config
 
-DB_USER = config('POSTGRES_USER', default='postgres')
-DB_PASSWORD = config('POSTGRES_PASSWORD', default='postgres')
-DB_NAME = config('POSTGRES_DB', default='FIAP')
-DB_HOST = config('POSTGRES_HOST', default='127.0.0.1')
+load_dotenv()
+
+#Pegando as configuracoes do .env
+DB_USER = config('POSTGRES_USER', default=os.getenv('POSTGRES_USER'))
+DB_PASSWORD = config('POSTGRES_PASSWORD', default=os.getenv('POSTGRES_PASSWORD'))
+DB_NAME = config('POSTGRES_DB', default=os.getenv('POSTGRES_DB'))
+DB_HOST = config('POSTGRES_HOST', default=os.getenv('POSTGRES_HOST'))
+#DB_PORT = config('POSTGRES_PORT', default=os.getenv('POSTGRES_PORT'))
 
 SQLALCHEMY_DATABASE_URL = (
-    "postgresql://postgres:postgres@gestao-postgresql:5432/FIAP"
+    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
 )
-#postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}
-# postgresql://postgres:postgres@localhost:5432/FIAP
-# jdbc:mysql://tc-soat3-db-svc/soat3?&createDatabaseIfNotExist=true
-# jdbc:postgresql://127.0.0.1:5432/
-# postgresql://postgres:postgres@gestao-postgresql:5432/FIAP?createDatabaseIfNotExist=true
+#"postgresql://postgres:postgres@gestao-postgresql:5432/FIAP"
 Base = declarative_base()
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
