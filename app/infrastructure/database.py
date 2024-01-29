c032225bb901e0c7db55631ba265027531bf319f
EDERSON RIBEIRO PAZ <edersonpaz.jbz@gmail.com>
2024-01-29 04:29:52 -0300

    Alteracao de pods
    correcao de arquivos

--- app/infrastructure/database.py
+++ app/infrastructure/database.py
@@ -9,12 +9,16 @@ from decouple import config
 DB_USER = config('POSTGRES_USER', default='postgres')
 DB_PASSWORD = config('POSTGRES_PASSWORD', default='postgres')
 DB_NAME = config('POSTGRES_DB', default='FIAP')
-DB_HOST = config('POSTGRES_HOST', default='gestao-postgresql')
+DB_HOST = config('POSTGRES_HOST', default='127.0.0.1')
 
 SQLALCHEMY_DATABASE_URL = (
-    f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+    "postgresql://postgres:postgres@gestao-postgresql:5432/FIAP"
 )
-
+#postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}
+# postgresql://postgres:postgres@localhost:5432/FIAP
+# jdbc:mysql://tc-soat3-db-svc/soat3?&createDatabaseIfNotExist=true
+# jdbc:postgresql://127.0.0.1:5432/
+# postgresql://postgres:postgres@gestao-postgresql:5432/FIAP?createDatabaseIfNotExist=true
 Base = declarative_base()
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
